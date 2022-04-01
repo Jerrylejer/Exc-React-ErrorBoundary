276a3d45d7769e2307c923a558902f2f035ef1a9
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-04-01 17:01:23 +0200

    upgrade React17 => React18

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
-// const container = document.getElementById('root');
-// const root = ReactDOM.createRoot(container);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-// root.render(
+root.render(<App />);
+
+// ReactDOM.render(
+//     <React.StrictMode>
 //         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
 // );
-
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
